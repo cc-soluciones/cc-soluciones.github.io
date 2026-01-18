70306b8b77e8216a30e819ebdcd2d844a841acf5
niba291 <nibachavez291@gmail.com>
2026-01-18 14:26:43 -0300

    feat: add navbar component

---
+++ LAND-PRO/src/constants/general.ts
@@ -0,0 +1,30 @@
+export const nav = [
+    {
+        label: "Home",
+        href: "#home",
+    },
+    {
+        label: "Servicios",
+        href: "#services",
+    },
+    {
+        label: "Proceso",
+        href: "#process",
+    },
+    {
+        label: "Contacto",
+        href: "#contact",
+    },
+    // {
+    //     label: "Stories",
+    //     href: "/stories",
+    // },
+    // {
+    //     label: "Pricing",
+    //     href: "/pricing",
+    // },
+    // {
+    //     label: "Docs",
+    //     href: "/docs",
+    // }
+];
