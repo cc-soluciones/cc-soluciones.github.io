4201b285455b31b8212d7c8fc59a6d38ad6066dc
Estebancardenas27 <estebancardenas2796@gmail.com>
2026-07-27 17:01:50 -0400

    feat: add new companies/logos

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,11 +1,12 @@
 export interface ProjectImage {
     src: string;
-    position: string;
+    position: string;    
 }
 
 export interface Project {
     images: ProjectImage[];
     url: string;
+    logo: string;
 }
 
 export const projects: Project[] = [
@@ -15,7 +16,8 @@ export const projects: Project[] = [
             { src: "projects/ccsoluciones_1_1.jpeg", position: "0% 0%" },
             { src: "projects/ccsoluciones_1_1.jpeg", position: "0% 110%" },
         ],
-        url: "http://www.blacker.ca",
+        url: "https://agrejur.com/",
+        logo: "/companies/agrejurlogo.jpeg",
     },
     {
         images: [
@@ -23,7 +25,8 @@ export const projects: Project[] = [
             { src: "projects/ccsoluciones_2_2.jpeg", position: "50% 0%" },
             { src: "projects/ccsoluciones_2_3.jpeg", position: "50% 110%" },
         ],
-        url: "https://pinguinoenlaroca.com",
+        url: "https://pro-losnotros.vercel.app",
+        logo: "/companies/losnotroslogo.jpeg",
     },
     {
         images: [
@@ -32,6 +35,7 @@ export const projects: Project[] = [
             { src: "projects/ccsoluciones_3_1.jpeg", position: "0% 110%" },
         ],
         url: "https://lapily.cl",
+        logo: "/companies/lapilylogo.webp",
     },
     {
         images: [
@@ -40,6 +44,7 @@ export const projects: Project[] = [
             { src: "projects/ccsoluciones_4_1.png", position: "0% 110%" },
         ],
         url: "https://denaval.cl",
+        logo: "/companies/denavallogo.png",
     },
     {
         images: [
@@ -48,6 +53,7 @@ export const projects: Project[] = [
             { src: "projects/ccsoluciones_5_1.jpeg", position: "0% 110%" },
         ],
         url: "https://www.electronicapowerch.com",
+        logo: "/companies/electronicaplogo.webp",
     },
     {
         images: [
@@ -56,5 +62,6 @@ export const projects: Project[] = [
             { src: "projects/ccsoluciones_6_1.png", position: "0% 110%" },
         ],
         url: "https://www.maquental.cl",
+        logo: "/companies/maquentallogo.jpg",
     },
 ];
